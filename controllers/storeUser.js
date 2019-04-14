5f1871ae6b8b5f7bb2409d8667a72dd7ef0cf37e
Vincent Occhiogrosso <16623151+vocchiogrosso@users.noreply.github.com>
2019-04-14 05:17:08 -0400

    B

--- controllers/storeUser.js
+++ controllers/storeUser.js
@@ -1,6 +1,21 @@
 const User = require('../database/models/User')
+const path = require('path')
 
 module.exports = (req, res) => {
+  /*
+    let image = req.files['uploads[]'];
+
+    image.mv(path.resolve(__dirname, '..', 'public/posts', image.name), (error) => {
+        Post.create({
+          ...req.body,
+          image: `/posts/${image.name}`,
+          verified: false,
+          author: req.session.userId
+        }, (error, post) => {
+          res.redirect("/");
+        });
+      })
+*/
     User.create(req.body, (error, user) => {
         if(error){
             const registrationErrors = Object.keys(error.errors).map(key => error.errors[key].message)
--- database/models/User.js
+++ database/models/User.js
@@ -5,16 +5,43 @@ const UserSchema = new mongoose.Schema({
     username: {
         type:String,
         required: [true,"Please Provide Your Username"],
+        unique: true
     },
     email: {
         type: String,
         required: [true,"Please Provide Your Email"],
         unique: true
     },
-    password: {
+    school: {
         type: String,
-        required: [true,"Please Provide Your Password"]
+        required: [true,"Please Provide The School You Currently Attend"]
+    },
+    year: {
+        type: String,
+        required: [true,"Please Provide Your Year In School"]
+    },
+    registered: {
+        type: Number,
+        required: [true,"How Many Hackathons Have You Registered And Confirmed Attendance To?"]
+    },
+    checkedin: {
+        type: Number,
+        required: [true,"How Many Hackathons Have You Checked-in/Showed Up To?"]
+    },
+    submitted: {
+        type: Number,
+        required: [true,"How Many Hackathons Have You Submitted Projects To?"]
+    },
+    won: {
+        type: Number,
+        required: [true,"How Many Hackathons Prizes Have You Won?"]
+    },
+    verified: Boolean,
+    createdAt: {
+        type: Date,
+        default: new Date()
     }
+
 })
 
 // Adds Middleware Before Continuing
@@ -22,10 +49,6 @@ UserSchema.pre('save', function(next){
     const user = this
     // 10, 100, 1000 for Rounds 
     // More Rounds Better But Slower
-    bcrypt.hash(user.password, 10, function(error, encrytped){
-        user.password = encrytped
-        next()
-    } )
 })
 
 module.exports = mongoose.model('User',UserSchema)
